2acfc515e7209578780e353db52ab8ec3994f5ad
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-10-17 17:22:08 +0900

    template corections

--- average_blur.py
+++ average_blur.py
@@ -2,47 +2,21 @@ from PIL import Image
 from math import pi, log, exp
 import numpy as np
 
-def blur_py(img, r):
+def average_blur(img, r):
     w, h = img.size
     a = np.array(img.getdata(), dtype=np.uint8).reshape(h, w)
     b = np.zeros((h,w), dtype=np.uint8)
     for i in range(h):
         for j in range(w):
-            s = 0.
-            up, bt = max(i-r,0), min(i+r+1,h)
-            lf, rt = max(j-r,0), min(j+r+1,w)
-            n = (bt-up)*(rt-lf)
-            for y in range(up,bt):
-                for x in range(lf,rt):
-                    s += a[y,x]
-            b[i,j] = s / n
+            s = a[i,j]+a[i-1,j]+a[i-2,j]
+            
+            # цикл по квадрату ±r?
+                    
+            b[i,j] = s / (2*r+1)**2
     return Image.fromarray(b)
 
 
-def blur_np(img, r):
-    w, h = img.size
-    a = np.array(img.getdata(), dtype=np.uint8)\
-                .reshape(h, w)
-    b = np.zeros((h,w), dtype=np.uint8)
-    for i in range(h):
-        for j in range(w):
-            up, bt = max(i-r,0), min(i+r+1,h)
-            lf, rt = max(j-r,0), min(j+r+1,w)
-            b[i,j] = np.average(a[up:bt, lf:rt])
-    return Image.fromarray(b)
 
-
-def blur_np(img, r):
-    w, h = img.size
-    a = np.array(img.getdata(), dtype=np.uint8)\
-                .reshape(h, w)
-    b = np.zeros((h,w), dtype=np.uint8)
-    for i in range(h):
-        for j in range(w):
-            up, bt = max(i-r,0), min(i+r+1,h)
-            lf, rt = max(j-r,0), min(j+r+1,w)
-            b[i,j] = np.average(a[up:bt, lf:rt])
-    return Image.fromarray(b)
 
 
 img = Image.open('darwin.png')
@@ -51,11 +25,6 @@ img.load()
 print("Image size is", img.size)
 print("Image mode is", img.mode)
 
-print("getdata[1]=", img.getdata()[1])
 a = np.array(img, dtype=np.uint8).reshape(img.size[::-1])
-print("a[0,1]=", a[0,1])
 
-b = a[900:1150, 900:1100]
-pic = Image.fromarray(b)
-
-blur_np(pic,16).show()
+blur_np(a,16).show()
