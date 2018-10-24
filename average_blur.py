87b4c86c44c28eaf750629e0f7bcc22e71e45322
tatakae1337 <36252142+tatakae1337@users.noreply.github.com>
2018-10-24 15:52:02 +0900

    Add files via upload

--- average_blur.py
+++ average_blur.py
@@ -6,17 +6,17 @@ def average_blur(img, r):
     w, h = img.size
     a = np.array(img.getdata(), dtype=np.uint8).reshape(h, w)
     b = np.zeros((h,w), dtype=np.uint8)
-    for i in range(h):
-        for j in range(w):
-            s = a[i,j]+a[i-1,j]+a[i-2,j]
+    for i in range(-r,h-r):
+        for j in range(-r,w-r):
+            s = 0.            
+            for x in range(-r,r):
+                for y in range(-r,r):
+                    s+=a[i-x,j-y]
             
-            # цикл по квадрату ±r?
+            # С†РёРєР» РїРѕ РєРІР°РґСЂР°С‚Сѓ В±r?
                     
             b[i,j] = s / (2*r+1)**2
     return Image.fromarray(b)
-
-
-
 
 
 img = Image.open('darwin.png')
@@ -25,6 +25,11 @@ img.load()
 print("Image size is", img.size)
 print("Image mode is", img.mode)
 
+print("getdata[1]=", img.getdata()[1])
 a = np.array(img, dtype=np.uint8).reshape(img.size[::-1])
+print("a[0,1]=", a[0,1])
 
-blur_np(a,16).show()
+b = a[900:1150, 900:1100]
+pic = Image.fromarray(b)
+
+blur_np(pic,16).show()
